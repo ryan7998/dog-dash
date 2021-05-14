57319d5771e2c300b6d64d1c6683bc2a7cb34ec8
fazle <ryan7998@gmail.com>
2021-05-14 18:09:20 -0400

    homepage banner component seperated

---
+++ client/src/components/HompageBanner/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+    Button,
+    Container,
+    Header,
+    Icon,
+} from 'semantic-ui-react';
+
+const HomepageBanner = () =>{
+    return(
+        <Container text>
+            <Header
+                as='h1'
+                content='Dog Dash'
+                style={{
+                fontSize: '4em',
+                fontWeight: 'normal',
+                marginBottom: 0,
+                marginTop: '3em',
+                }}
+            />
+            <Header
+                as='h2'
+                content='Do whatever you want when you want to.'
+                style={{
+                fontWeight: 'normal',
+                }}
+            />
+                    <Link to="/login">
+            <Button color='teal' size='huge'>
+                Register
+                <Icon name='right arrow' />
+            </Button>
+            </Link>
+            </Container>
+    )
+}
+
+export default HomepageBanner;
---
+++ client/src/pages/JobLists.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import JobList from '../components/JobList';
+
+const JobLists = () =>{
+
+    return(
+        <JobList />
+    )
+}
+export default JobLists
