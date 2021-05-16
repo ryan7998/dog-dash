fe59a838923f0bba8dc34e7197115e2f94866a57
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-05-15 22:27:34 -0400

    filtering user by id added

--- client/src/components/WalkerJobsComp/index.js
+++ client/src/components/WalkerJobsComp/index.js
@@ -1,20 +1,28 @@
 import React, { useEffect } from "react";
 import JobItem from "../JobItem";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 //import { useStoreContext } from "../../utils/GlobalState";
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import { UPDATE_JOBS } from "../../utils/actions";
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_JOBS } from "../../utils/queries";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_JOB_BYID } from "../../utils/queries";
 import { idbPromise } from "../../utils/helpers";
-import spinner from "../../assets/spinner.gif"
-import { Card, Icon } from 'semantic-ui-react';
-
+import spinner from "../../assets/spinner.gif";
+import { Card, Icon } from "semantic-ui-react";
+import decode from "jwt-decode";
 
 function WalkerJobs(props) {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
-  const { loading, data } = useQuery(QUERY_JOBS);
+
+  //getuser id
+  const token = localStorage.getItem("id_token");
+  const walkerId = decode(token);
+  //console.log("decoded : ", decoded.data._id);
+
+  const { loading, data } = useQuery(QUERY_JOB_BYID, {
+    variables: { id: walkerId },
+  });
 
   useEffect(() => {
     if (data) {
@@ -35,34 +43,35 @@ function WalkerJobs(props) {
     }
   }, [data, loading, dispatch]);
 
-  function filterJobs() {  
-    return state.jobs.filter(job => job.status === props.status);
+  function filterJobs() {
+    //console.log(state.jobs);
+    return state.jobs.filter((job) => job.appliedUsers.length > 0);
   }
 
   //It's not executed yet
-  function alertFunction(id){
+  function alertFunction(id) {
     alert("Is this is? : " + id);
   }
 
   return (
     <div>
-      <h2 onClick='alertFunction({job._id})'></h2>
+      <h2 onClick="alertFunction({job._id})"></h2>
       <h2>Our Jobs:</h2>
       {state.jobs.length ? (
         <Card.Group itemsPerRow={3}>
           {filterJobs().map((job) => (
             <Link to={`/singlewalkerjob/${job._id}`}>
-            <JobItem
-              key={job._id}
-              _id={job._id}
-              user_id={job.user_id}
-              title={job.title}
-              description={job.description}
-              price={job.price}
-              date={job.date}
-              status={job.status}
-              image={job.image}
-            />
+              <JobItem
+                key={job._id}
+                _id={job._id}
+                user_id={job.user_id}
+                title={job.title}
+                description={job.description}
+                price={job.price}
+                date={job.date}
+                status={job.status}
+                image={job.image}
+              />
             </Link>
           ))}
         </Card.Group>
