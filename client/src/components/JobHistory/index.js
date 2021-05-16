9637e567a51849bd6f5d7290521b52a84b4a54aa
Johann Taylor <j.taylor1343@gmail.com>
2021-05-16 15:24:13 -0400

    Updated styling

---
+++ client/src/components/JobHistory/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+ function LiveJob() {
+
+
+    
+    return (
+    <div>
+        <div class="ui items">
+        <div class="item">
+            <div class="image">
+                <img src="/images/wireframe/image.png"/>
+            </div>
+        <div class="content">
+      <a class="header">Header</a>
+      <div class="meta">
+        <span>Description</span>
+      </div>
+      <div class="description">
+        <p></p>
+      </div>
+      <div class="extra">
+        Additional Details
+      </div>
+    </div>
+  </div>
+  
+    </div>
+  </div>
+
+    )
+}
+
+export default LiveJob
