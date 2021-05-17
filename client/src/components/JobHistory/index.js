ae61e0589edcbd5af3088bf0997aaa98f58056bc
Johann Taylor <j.taylor1343@gmail.com>
2021-05-17 14:35:28 -0400

    Style updated

--- client/src/components/JobHistory/index.js
+++ client/src/components/JobHistory/index.js
@@ -12,9 +12,6 @@ function JobHistory() {
     <div>
         <div class="ui items">
         <div class="item">
-            <div class="image">
-                <img src="/images/wireframe/image.png"/>
-            </div>
         <div class="content">
       <a class="header">Header</a>
       <div class="meta">
