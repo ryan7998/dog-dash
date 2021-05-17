f7a661c7ff3834d89d6d849f8039296c5ffbcaff
Johann Taylor <j.taylor1343@gmail.com>
2021-05-17 19:39:07 -0400

    Styling update

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -14,10 +14,9 @@ import {
 function Profile(){
   return (
     <div>
-    <div className="profile-page">
-      <ProfilePage type="Dog Walker"/>
-      <JobHistory type="Dog Walker"/>
-    </div>
+      <div className="profile-page">
+        <ProfilePage type="Dog Walker"/>
+      </div>
      </div>
   );
 };
