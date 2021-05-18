601ad127149879703bb8f4e495ee9070b054f1bb
fazle <ryan7998@gmail.com>
2021-05-18 18:51:32 -0400

    profile for owner and walker

--- client/src/components/UserProfileDetails/index.js
+++ client/src/components/UserProfileDetails/index.js
@@ -21,26 +21,13 @@ const UserProfileDetails = ({profileData}) =>{
         orders
     } = profileData;
 
-    // console.log(_id,
-    //     firstName,
-    //     lastName,
-    //     email,
-    //     address,
-    //     description,
-    //     image,
-    //     ratingAvg,
-    //     type,
-    //     submittedJobs,
-    //     appliedJobs,
-    //     selectedJobs,
-    //     orders);
+    // get the type of user:
     const userIsWalker = (type === 'Dog Walker');
     
-    console.log('selectedJobs: ', selectedJobs);
-
   return (
       <>
     <div className="flex-row">
+    
       <ProfileData
         key={_id}
         _id={_id}
@@ -55,11 +42,13 @@ const UserProfileDetails = ({profileData}) =>{
         hideJobButton = {true}
       />
     </div>
+    {/* if user is a walker show number of jobs completed: */}
     {userIsWalker && (
         <div className="flex-row">
             Total Jobs, {firstName} Completed: <b>{selectedJobs.length}</b>
         </div>
     )}
+    {/* if user is owner show number of posts: */}
     {!userIsWalker && (
         <div>Total Hires: {orders.length}</div>
     )}
